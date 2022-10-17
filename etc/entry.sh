47b6b85d5a35f948749e6e8ad30550ca7154ba31
maddinthe <martintheilen@gmail.com>
2022-10-17 14:17:32 +0200

    Changes to be committed: 	modified:   dockerfile 	modified:   etc/entry.sh

--- etc/entry.sh
+++ etc/entry.sh
@@ -14,19 +14,6 @@ if [ ! -f "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg" ]; then
 	# Change hostname on first launch (you can comment this out if it has done its purpose)
 	sed -i -e 's/{{SERVER_HOSTNAME}}/'"${SRCDS_HOSTNAME}"'/g' "${STEAMAPPDIR}/${STEAMAPP}/cfg/server.cfg"
 fi
-
-# Are we in a metamod container and is the metamod folder missing?
-if  [ ! -z "$METAMOD_VERSION" ] && [ ! -d "${STEAMAPPDIR}/${STEAMAPP}/addons/metamod" ]; then
-	LATESTMM=$(wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/mmsource-latest-linux)
-	wget -qO- https://mms.alliedmods.net/mmsdrop/"${METAMOD_VERSION}"/"${LATESTMM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"	
-fi
-
-# Are we in a sourcemod container and is the sourcemod folder missing?
-if  [ ! -z "$SOURCEMOD_VERSION" ] && [ ! -d "${STEAMAPPDIR}/${STEAMAPP}/addons/sourcemod" ]; then
-	LATESTSM=$(wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/sourcemod-latest-linux)
-	wget -qO- https://sm.alliedmods.net/smdrop/"${SOURCEMOD_VERSION}"/"${LATESTSM}" | tar xvzf - -C "${STEAMAPPDIR}/${STEAMAPP}"
-fi
-
 # Believe it or not, if you don't do this srcds_run shits itself
 cd "${STEAMAPPDIR}"
 
@@ -51,55 +38,29 @@ sv_password	+sv_password "${SRCDS_PW}"
 rcon_password	+rcon_password "${SRCDS_RCONPW}"
 EOM
 	# if autoexec is present, drop overwritten arguments here (example: SRCDS_PW & SRCDS_RCONPW)
-	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
-				-steam_dir "${STEAMCMDDIR}" \
-				-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
-				-usercon \
-				+fps_max "${SRCDS_FPSMAX}" \
-				-tickrate "${SRCDS_TICKRATE}" \
-				-port "${SRCDS_PORT}" \
-				+tv_port "${SRCDS_TV_PORT}" \
-				+clientport "${SRCDS_CLIENT_PORT}" \
-				-maxplayers_override "${SRCDS_MAXPLAYERS}" \
-				+game_type "${SRCDS_GAMETYPE}" \
-				+game_mode "${SRCDS_GAMEMODE}" \
-				+mapgroup "${SRCDS_MAPGROUP}" \
-				+map "${SRCDS_STARTMAP}" \
-				+sv_setsteamaccount "${SRCDS_TOKEN}" \
-				+sv_region "${SRCDS_REGION}" \
-				+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
-				-ip "${SRCDS_IP}" \
-				+sv_lan "${SRCDS_LAN}" \
-				+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
-				+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
-				-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
+	bash "${STEAMAPPDIR}/svends_run" -console -autoupdate \
+				+fps_max "${SVENDS_FPSMAX}" \
+				+sys_ticrate "${SVENDS_TICKRATE}" \
+				-port "${SVENDS_PORT}" \
+				+maxplayers "${SVENDS_MAXPLAYERS}" \
+				+map "${SVENDS_STARTMAP}" \
+				+sv_region "${SVENDS_REGION}" \
+				+ip "${SVENDS_IP}" \
+				+sv_lan "${SVENDS_LAN}" \
 				"${ow_args}" \
 				"${ADDITIONAL_ARGS}"
 else
 	# If no autoexec is present, use all parameters
-	bash "${STEAMAPPDIR}/srcds_run" -game "${STEAMAPP}" -console -autoupdate \
-				-steam_dir "${STEAMCMDDIR}" \
-				-steamcmd_script "${HOMEDIR}/${STEAMAPP}_update.txt" \
-				-usercon \
-				+fps_max "${SRCDS_FPSMAX}" \
-				-tickrate "${SRCDS_TICKRATE}" \
-				-port "${SRCDS_PORT}" \
-				+tv_port "${SRCDS_TV_PORT}" \
-				+clientport "${SRCDS_CLIENT_PORT}" \
-				-maxplayers_override "${SRCDS_MAXPLAYERS}" \
-				+game_type "${SRCDS_GAMETYPE}" \
-				+game_mode "${SRCDS_GAMEMODE}" \
-				+mapgroup "${SRCDS_MAPGROUP}" \
-				+map "${SRCDS_STARTMAP}" \
-				+sv_setsteamaccount "${SRCDS_TOKEN}" \
-				+rcon_password "${SRCDS_RCONPW}" \
-				+sv_password "${SRCDS_PW}" \
-				+sv_region "${SRCDS_REGION}" \
-				+net_public_adr "${SRCDS_NET_PUBLIC_ADDRESS}" \
-				-ip "${SRCDS_IP}" \
-				+sv_lan "${SRCDS_LAN}" \
-				+host_workshop_collection "${SRCDS_HOST_WORKSHOP_COLLECTION}" \
-				+workshop_start_map "${SRCDS_WORKSHOP_START_MAP}" \
-				-authkey "${SRCDS_WORKSHOP_AUTHKEY}" \
+	bash "${STEAMAPPDIR}/svends_run" -console \
+				+fps_max "${SVENDS_FPSMAX}" \
+				+sys_ticrate "${SVENDS_TICKRATE}" \
+				-port "${SVENDS_PORT}" \
+				+maxplayers "${SVENDS_MAXPLAYERS}" \
+				+map "${SVENDS_STARTMAP}" \
+				+rcon_password "${SVENDS_RCONPW}" \
+				+sv_password "${SVENDS_PW}" \
+				+sv_region "${SVENDS_REGION}" \
+				-ip "${SVENDS_IP}" \
+				+sv_lan "${SVENDS_LAN}" \
 				"${ADDITIONAL_ARGS}"
 fi
